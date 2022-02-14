bb103c415b85dfa5ec32f24e1865d62777742e6c
Joonwoo Kwon <pioneers@snu.ac.kr>
2022-02-14 13:06:46 +0900

    train.py updated.

--- train.py
+++ train.py
@@ -1,40 +1,41 @@
-# 라이브러리 추가하기
+## 라이브러리 추가하기
 import os
 import numpy as np
 
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
-
+#from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 from torchvision import transforms, datasets
 
-## 트레이닝을 위한 파라미터를 설정하기
+## 트레이닝 필요한 파라메터를 설정하기
 lr = 1e-3
 batch_size = 64
 num_epoch = 10
 
-ckpt_dir = './checkpoint' # 학습된 네트워크 저장
-log_dir = './log' # tensorboard 파일 저장
+ckpt_dir = './checkpoint'
+log_dir = './log'
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 ## 네트워크를 구축하기
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.convl = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5, stride=1, padding=0, bias=True)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5, stride=1, padding=0, bias=True)
         self.pool1 = nn.MaxPool2d(kernel_size=2)
-        self_relu1 = nn.Relu()
+        self.relu1 = nn.ReLU()
 
-        self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, stride =1, padding==0, bias = True)
+        self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, stride=1, padding=0, bias=True)
         self.drop2 = nn.Dropout2d(p=0.5)
         self.pool2 = nn.MaxPool2d(kernel_size=2)
-        self.relu2 = nn.Relu()
+        self.relu2 = nn.ReLU()
 
         self.fc1 = nn.Linear(in_features=320, out_features=50, bias=True)
-        self.relu1_fc1 = nn.Relu()
+        self.relu1_fc1 = nn.ReLU()
         self.drop1_fc1 = nn.Dropout2d(p=0.5)
 
         self.fc2 = nn.Linear(in_features=50, out_features=10, bias=True)
@@ -49,7 +50,7 @@ class Net(nn.Module):
         x = self.pool2(x)
         x = self.relu2(x)
 
-        x = x.view(-1,320)
+        x = x.view(-1, 320)
 
         x = self.fc1(x)
         x = self.relu1_fc1(x)
@@ -59,49 +60,51 @@ class Net(nn.Module):
 
         return x
 
+
 ## 네트워크를 저장하거나 불러오는 함수 작성하기
 def save(ckpt_dir, net, optim, epoch):
-    if not os.path.exisits(ckpt_dir):
-        os.makedir(ckpt_dir)
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
 
-        torch.save({{'net': net.state_dict(), 'optim': optim.state_dict()},
-                    './%s/model_epoch%d.pth'%(ckpt_dir, epoch)})
+    torch.save({'net': net.state_dict(), 'optim': optim.state_dict()},
+               './%s/model_epoch%d.pth' % (ckpt_dir, epoch))
+
 
 def load(ckpt_dir, net, optim):
     ckpt_lst = os.listdir(ckpt_dir)
     ckpt_lst.sort()
 
-    dict_model = torch.load('./%s/%s'% (ckpt_dir, ckpt_list[-1]))
+    dict_model = torch.load('./%s/%s' % (ckpt_dir, ckpt_lst[-1]))
 
     net.load_state_dict(dict_model['net'])
     optim.load_state_dict(dict_model['optim'])
 
     return net, optim
 
+
 ## MNIST 데이터 불러오기
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std = (0.5,))])
-
-dataset = datasets.MNIST(download=True, root ='./', train=True, transforms=transform)
+dataset = datasets.MNIST(download=True, root='./', train=True, transform=transform)
 loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
 num_data = len(loader.dataset)
 num_batch = np.ceil(num_data / batch_size)
 
-## 네트워크 설정 및 필요한 loss function 구현하기
+## 네트워크 설정 및 필요한 손실함수 구현하기
 net = Net().to(device)
 params = net.parameters()
 
 fn_loss = nn.CrossEntropyLoss().to(device)
 fn_pred = lambda output: torch.softmax(output, dim=1)
-fn_acc = lambda pred, label = ((pred.max(dim=1)[1] == label).type(torch.float)).mean()
+fn_acc = lambda pred, label: ((pred.max(dim=1)[1] == label).type(torch.float)).mean()
 
-optim = torch.optim.Adam(params, lr = lr)
+optim = torch.optim.Adam(params, lr=lr)
 
-writer = SummaryWriter(log_dir = log_dir)
+writer = SummaryWriter(log_dir=log_dir)
 
 ## 트레이닝 시작하기
-for epoch in range(1, num_epoch+1):
+for epoch in range(1, num_epoch + 1):
     net.train()
 
     loss_arr = []
@@ -113,7 +116,6 @@ for epoch in range(1, num_epoch+1):
 
         output = net(input)
         pred = fn_pred(output)
-
 
         optim.zero_grad()
 
@@ -127,15 +129,10 @@ for epoch in range(1, num_epoch+1):
         loss_arr += [loss.item()]
         acc_arr += [acc.item()]
 
-        print('Train: Epoch %04d/%04d | BATCH %04d/%04d | LOSS: %.4f | ACC %.4f' %
+        print('TRAIN: EPOCH %04d/%04d | BATCH %04d/%04d | LOSS: %.4f | ACC %.4f' %
               (epoch, num_epoch, batch, num_batch, np.mean(loss_arr), np.mean(acc_arr)))
 
     writer.add_scalar('loss', np.mean(loss_arr), epoch)
     writer.add_scalar('acc', np.mean(acc_arr), epoch)
 
-    save(ckpt_dir = ckpt_dir, net = net, optim = optim, epoch = epoch)
-
-writer.close()
-
-
-
+    save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
